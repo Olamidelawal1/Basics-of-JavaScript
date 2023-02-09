39cb75bf20231a3251bd08e53674f24fa2b47b16
Olamide Lawal <105231581+Olamidelawal1@users.noreply.github.com>
2023-02-09 13:11:23 +0100

    Add files via upload
    Initial commit

---
+++ js-basics/index.js
@@ -0,0 +1,22 @@
+//Function that multiples two numbers
+
+function multiply (number1, number2){
+    return number1 * number2
+}
+console.log (multiply(5,5))
+
+//Creating variables
+let firstName = "Olamide ";
+let lastName = "Lawal";
+let stuternTrack = "Frontend web development";
+let skills = "good communication skills";
+let favouriteColor = "black";
+let CountryIndependence = "1960";
+console.log ("My name is " + firstName + lastName + ". " + "I'm currently in the " + stuternTrack + " track." + 
+    "I have " +skills + ", " + "My favourite colour is " + favouriteColor + " and my country gained its independence in the year " 
+    + CountryIndependence + ".")
+
+
+
+
+
